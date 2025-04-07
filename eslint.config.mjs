ee2ddf9bfc280ddf464967e12efc28d979d93e60
ftzi <henrique.bruno.fa@gmail.com>
2025-04-08 00:19:04 +0200

    setup Biome

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
+import biome from "eslint-config-biome";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  biome,
 ];
 
 export default eslintConfig;
