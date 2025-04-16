147dc6aa546f3cb847a1a0c53a733fc07dda8989
Thiago Murtinho <thiago.murtinho@live.com>
2025-04-16 19:18:05 -0300

    feat(home): simplify Home component to redirect to the first page 🚀

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,5 @@
-import { CreateEventButton } from "@/components/events/create-event-button";
-import EventsList from "@/components/events/events-list";
-import EventsListSkeleton from "@/components/events/events-list-skeleton";
-import SearchInput from "@/components/search-input";
-import { Suspense } from "react";
+import { redirect } from "next/navigation";
 
-interface HomePageProps {
-  searchParams: {
-    query?: string;
-    page?: string;
-  };
-}
-
-export default function Home({ searchParams }: HomePageProps) {
-  const query = searchParams.query || "";
-  const currentPage = Number(searchParams.page) || 1;
-
-  return (
-    <div className="flex min-h-screen flex-col">
-      <section className="flex-1 container mx-auto">
-        <div className="py-6 md:py-12">
-          <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-8">
-            <div>
-              <h1 className="text-3xl font-bold tracking-tight">
-                Hackathon Events
-              </h1>
-              <p className="text-muted-foreground mt-1">
-                Manage your hackathon events and configurations
-              </p>
-            </div>
-            <div className="flex flex-col sm:flex-row w-full md:w-auto gap-4 mt-4 md:mt-0">
-              <div className="relative w-full sm:w-64">
-                <SearchInput defaultValue={query} />
-              </div>
-              <CreateEventButton />
-            </div>
-          </div>
-          <Suspense fallback={<EventsListSkeleton />}>
-            <EventsList
-              searchQuery={query}
-              currentPage={currentPage}
-            />
-          </Suspense>
-        </div>
-      </section>
-    </div>
-  );
+export default function Home() {
+  redirect("/page/1");
 }
